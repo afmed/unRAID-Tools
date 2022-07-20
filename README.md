1a358844e01223ad730032e8ac0659c42286163f
afmed <github@theaftab.com>
2022-07-20 21:29:51 +0100

    modded for QNAP TS-EC2480U backwards control

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # unRAID-Tools
 A collection of tools for unRAID
+
+
+Modified to work with the QNAP TS-EC2480U
+
+needs the following module loaded by adding it to /boot/config/go
+```
+modprobe nct7802
+```
+
+Auto fan control in the BIOS also needs to be disabled
+
+
--- unraid_array_fan.sh
+++ unraid_array_fan.sh
@@ -8,6 +8,7 @@
 #        was returning two numbers with just "Temperature".
 # v0.5: By Pauven:  Added linear PWM logic to slowly ramp speed when fan is between HIGH and OFF.
 # v0.6: By kmwoley: Added fan start speed. Added logging, suppressed unless fan speed is changed.
+# v0.7: By afmed: Udated to work with the backwards QNAP TS-EC2480U 
 # A simple script to check for the highest hard disk temperatures in an array
 # or backplane and then set the fan to an apropriate speed. Fan needs to be connected
 # to motherboard with pwm support, not array.
@@ -15,7 +16,7 @@
 
 ### VARIABLES FOR USER TO SET ###
 # Amount of drives in the array. Make sure it matches the amount you filled out below.
-NUM_OF_DRIVES=5
+NUM_OF_DRIVES=22
 
 # unRAID drives that are in the array/backplane of the fan we need to control
 HD[1]=/dev/sdb
@@ -23,29 +24,29 @@ HD[2]=/dev/sdc
 HD[3]=/dev/sdd
 HD[4]=/dev/sde
 HD[5]=/dev/sdf
-#HD[6]=/dev/sdg
-#HD[7]=/dev/sdh
-#HD[8]=/dev/sdi
-#HD[9]=/dev/sdj
-#HD[10]=/dev/sdk
-#HD[11]=/dev/sdl
-#HD[12]=/dev/sdm
-#HD[13]=/dev/sdn
-#HD[14]=/dev/sdo
-#HD[15]=/dev/sdp
-#HD[16]=/dev/sdq
-#HD[17]=/dev/sdr
-#HD[18]=/dev/sds
-#HD[19]=/dev/sdt
-#HD[20]=/dev/sdu
-#HD[21]=/dev/sdv
-#HD[22]=/dev/sdw
+HD[6]=/dev/sdg
+HD[7]=/dev/sdh
+HD[8]=/dev/sdi
+HD[9]=/dev/sdj
+HD[10]=/dev/sdk
+HD[11]=/dev/sdl
+HD[12]=/dev/sdm
+HD[13]=/dev/sdn
+HD[14]=/dev/sdo
+HD[15]=/dev/sdp
+HD[16]=/dev/sdq
+HD[17]=/dev/sdr
+HD[18]=/dev/sds
+HD[19]=/dev/sdt
+HD[20]=/dev/sdu
+HD[21]=/dev/sdv
+HD[22]=/dev/sdw
 #HD[23]=/dev/sdx
 #HD[24]=/dev/sdy
 
 # Temperatures to change fan speed at
 # Any temp between OFF and HIGH will cause fan to run on low speed setting 
-FAN_OFF_TEMP=35     # Anything this number and below - fan is off
+FAN_OFF_TEMP=25     # Anything this number and below - fan is off
 FAN_HIGH_TEMP=45    # Anything this number or above - fan is high speed
 
 # Fan speed settings. Run pwmconfig (part of the lm_sensors package) to determine 
@@ -55,17 +56,22 @@ FAN_HIGH_TEMP=45    # Anything this number or above - fan is high speed
 # (Default: 255 to be safe).
 # Any real number between 0 and 255.
 #
-FAN_OFF_PWM=0
-FAN_LOW_PWM=100
-FAN_START_PWM=255
-FAN_HIGH_PWM=255
+#FAN_OFF_PWM=0
+#FAN_LOW_PWM=100
+#FAN_START_PWM=255
+#FAN_HIGH_PWM=255
+
+FAN_OFF_PWM=255    # Fan off value
+FAN_START_PWM=100  # to get fan spinning
+FAN_LOW_PWM=190    # minimum speed
+FAN_HIGH_PWM=100   # max speed
 
 # Fan device. Depends on your system. pwmconfig can help with finding this out. 
 # pwm1 is usually the cpu fan. You can "cat /sys/class/hwmon/hwmon0/device/fan1_input"
 # or fan2_input and so on to see the current rpm of the fan. If 0 then fan is off or 
 # there is no fan connected or motherboard can't read rpm of fan.
 # ARRAY_FAN=/sys/class/hwmon/hwmon1/device/pwm2
-ARRAY_FAN=/sys/class/hwmon/hwmon2/pwm1
+ARRAY_FAN=/sys/devices/pci0000\:00//0000\:00\:1f.3/i2c-0/0-002b/hwmon/hwmon3/pwm3
 
 ### END USER SET VARIABLES ###
 
@@ -132,8 +138,8 @@ fi
 # produce output if the fan speed was changed
 CURRENT_SPEED=`cat $ARRAY_FAN`
 if [ "$PREVIOUS_SPEED" -ne "$CURRENT_SPEED" ]; then
-  echo "Fan speed has changed."
-  echo "${OUTPUT}"
+  echo "Fan speed has changed." | /usr/bin/logger -t fan_control
+  echo "${OUTPUT}" | /usr/bin/logger -t fan_control
 else
-  echo "Fan speed unchanged. Highest temp: "$HIGHEST_TEMP" Current pwm: "$CURRENT_SPEED
+  echo "Fan speed unchanged. Highest temp: "$HIGHEST_TEMP" Current pwm: "$CURRENT_SPEED | /usr/bin/logger -t fan_control
 fi
